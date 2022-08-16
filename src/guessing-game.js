2a599480828399a93fbac19f0eb3ebf897adf9b9
Kirill <bloodwingest@gmail.com>
2022-08-17 00:18:58 +0300

    feat: solved the task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
+        this.result = 0;
     }
 
     guess() {
-
-    }
-
-    lower() {
-
+        this.result =  Math.round((this.maxValue+this.minValue) / 2)
+        return this.result;
     }
 
     greater() {
+        return this.minValue = Math.round(this.result)
+    }
 
+    lower() {
+        return this.maxValue = Math.round(this.result)
     }
 }
 
